a2b1ce884f0d016e185a7b04095bedd07ad719db
Jamey Wicklund <theBluWiz@icloud.com>
2022-11-08 19:02:00 -0800

    Updates API Get and adds iteration for DOM updates

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,13 +3,19 @@ var definitionArray = "";
 var giphArray = [];
 let wordInputEl = document.getElementById("js-word-input");
 let wordSearchButton = document.getElementById("js-search-word");
-let wordDefinition = document.getElementById("word-definiton");
+let wordDefinition = document.getElementById("word-definition");
 let giphDisplayEl = document.getElementById("giphs");
 
-var meriamWebsterURL = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${searchedWord}?key=35209bec-5678-4beb-85bc-14642a260055`;
-console.log(meriamWebsterURL)
+function meriamWebsterURL() {
+  let url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${searchedWord}?key=35209bec-5678-4beb-85bc-14642a260055`;
+  console.log(url)
+  return url
+}
 
-var giphyURL = `https://api.giphy.com/v1/gifs/search?api_key=C46PLya5FW7iVdgTbVrt2tvX26ZgIo8w&q=${searchedWord}&limit=3&offset=0&rating=g&lang=en`
+function giphyURL() {
+  let url = `https://api.giphy.com/v1/gifs/search?api_key=C46PLya5FW7iVdgTbVrt2tvX26ZgIo8w&q=${searchedWord}&limit=3&offset=0&rating=g&lang=en`
+  return url
+}
 
 // link for search history 
 function getWord(event) {
@@ -33,17 +39,27 @@ function getAPI(requestUrl) {
       if (requestUrl === meriamWebsterURL) {
         definitionArray = data[0].shortdef
         console.log(definitionArray)
+        for (let i = 0; i < definitionArray; i++) {
+          let definition = document.createElement("p")
+          definition.textContent = definitionArray[i]
+          wordDefinition.appendChild(definition)
+        }
       }
       if (requestUrl === giphyURL) {
         giphArray = data.data;
         console.log(giphArray)
+        for (let i = 0; i < giphArray; i++) {
+          let giph = document.createElement("iframe");
+          giph.src = giphArray[i].url;
+          giphDisplayEl.appendChild(giph)
+        }
       }
     });
 }
 
-submitScoreEl.addEventListener("click", function (event) {
+wordSearchButton.addEventListener("click", function (event) {
   event.preventDefault();
   searchedWord = wordInputEl.value
-  getAPI(meriamWebsterURL)
-  getAPI(giphyURL)
+  getAPI(meriamWebsterURL())
+  getAPI(giphyURL())
 });
