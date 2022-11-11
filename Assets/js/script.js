595a16bf1b2e021fd269dad57d5f250cb6bca9f1
Jamey Wicklund <theBluWiz@icloud.com>
2022-11-10 19:15:05 -0800

    Restores functionality of website after earlier merges

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,8 +8,6 @@ let wordInputEl = document.getElementById("search-modal");
 let wordSearchButton = document.getElementById("searchBtn");
 let wordDefinition = document.getElementById("wordDefinition");
 let giphDisplayEl = document.getElementById("giphs");
-console.log(wordDefinition)
-console.log(giphDisplayEl)
 
 // State Variables
 var searchedWord = "";
